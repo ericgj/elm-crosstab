6569c3d42c700c87a827db17b16d5372c830db9d
Eric Gjertsen <ericgj72@gmail.com>
2017-07-20 23:35:13 -0400

    first draft documentation (not finished)
    exposing accessors
    
    add levelMap

--- src/Crosstab.elm
+++ src/Crosstab.elm
@@ -5,11 +5,16 @@ module Crosstab
         , LevelMap
         , Compare
         , Levels
-        , Values
-        , Summary
         , fromList
         , fromListWithLevels
         , levelsOf
+        , levelMap
+        , rowLevelList
+        , colLevelList
+        , rowList
+        , rowSummaryList
+        , colSummaryList
+        , tableSummary
         , calc
         , compare
         , compareAndCalc
@@ -19,6 +24,40 @@ module Crosstab
         , mapCalc2
         )
 
+{-| 
+A library for calculating crosstab (AKA contingency) tables.
+
+Built on top of [elm-flat-matrix][].
+
+# Constructing
+
+@docs fromList, fromListWithLevels, levelsOf, levelMap, Levels
+
+
+# Getting data out
+
+@docs levels, rowLevelList, colLevelList, rowList, rowSummaryList, colSummaryList, tableSummary
+
+
+# Calculating
+
+@docs calc, compare, compareAndCalc, Compare
+
+
+# Defining calculations
+
+@docs customCalc, mapCalcOf, mapCalc, mapCalcOf2, mapCalc2
+
+
+# Internal types
+
+@docs Crosstab, LevelMap, Calc, Levels, Values, Summary
+
+
+[elm-flat-matrix]: https://package.elm-lang.org/packages/eeue56/elm-flat-matrix/latest
+
+-}
+
 import Matrix exposing (Matrix)
 import Array exposing (Array)
 import Dict exposing (Dict)
@@ -26,6 +65,12 @@ import Set exposing (Set)
 import Tuple
 
 
+{-|
+
+Internal structure of a crosstab table. See `fromList` and `fromListWithLevels`
+for how to construct a Crosstab from source data.
+
+-}
 type Crosstab a b comparable1 comparable2
     = Crosstab
         { levels : Levels comparable1 comparable2
@@ -34,6 +79,12 @@ type Crosstab a b comparable1 comparable2
         }
 
 
+{-|
+
+Internal structure of a calculation. See prebuilt calculations in 
+`Crosstab.Calc` and `Crosstab.Stats`, or construct your own with `customCalc`.
+
+-}
 type Calc a b c
     = Calc
         { map : b -> c
@@ -42,13 +93,30 @@ type Calc a b c
         }
 
 
+{-|
+
+Internal mapping from source data to row and column levels. Use `levelMap` to
+construct.
+
+-}
 type LevelMap a comparable1 comparable2
     = LevelMap
         { row : a -> comparable1
         , col : a -> comparable2
         }
 
+{-|
 
+Summary data for calculations. See `compare` for usage example.
+
+  - **table** is the overall table summary 
+  - **row** is the row summary for the current value
+  - **col** is the column summary for the current value
+  - **prevRow** is the value at the previous row, or `Nothing` if the first row
+  - **prevCol** is the value at the previous column, or `Nothing` if the first
+    column
+
+-}
 type alias Compare a b =
     { table : b
     , row : b
@@ -57,7 +125,11 @@ type alias Compare a b =
     , prevCol : Maybe a
     }
 
+{-|
 
+Row and column levels. See `fromListWithLevels` for usage example.
+
+-}
 type alias Levels comparable1 comparable2 =
     { rows : Array comparable1
     , cols : Array comparable2
@@ -78,7 +150,38 @@ type alias Summary a =
 
 -- CONSTRUCTING
 
+{-|
 
+Load and calculate a crosstab table from source data given 
+
+  - a _summary_ calculation, 
+  - a _value_ calculation, and
+  - a mapping of row and column levels from source data.
+
+Assuming you have a list of records where `x1` is your row category, `x2` is 
+your column category, and `y` is your value, this will calculate a crosstab
+where the _values_ in the table are the sums of y, and the _summary_ of the
+rows, columns, and overall table are the means of those sums.
+
+    fromList 
+        (Crosstab.Stats.summary .mean) 
+        (Crosstab.Stats.summaryOf .y .sum)
+        (levelMap { row = .x1, col = .x2 })
+        data
+
+Please note there is some subtlety in the structure of the _value_ vs.
+_summary_ calculations. The former _accumulates_ source records while the
+latter _adds_ together previous accumulations.  The type signatures help
+tell the story.
+
+If you are using prebuilt calculations from `Crosstab.Calc` or
+`Crosstab.Stats`, the main thing to be aware of is the functions ending in
+`Of` define _value_ calculations, while those that don't end in `Of` define
+_summary_ calculations.
+
+For more details, see [Defining calculations](#defining-calculations).
+
+-} 
 fromList :
     Calc b b d
     -> Calc a b c
@@ -93,7 +196,12 @@ fromList summary value map records =
         map
         records
 
+{-|
 
+Like `fromList`, but specifying row and column levels explicitly. This saves
+one pass through the source data, if you know them up front.
+
+-}
 fromListWithLevels :
     Levels comparable1 comparable2
     -> Calc b b d
@@ -139,6 +247,14 @@ fromListWithLevels { rows, cols } summary (Calc value) (LevelMap { row, col }) r
             |> finalize
 
 
+{-|
+
+Extract the unique row and column levels from source data, given a _level 
+mapping_. Used internally by `fromList`, but can be used generally.
+
+    levelsOf (levelMap { row = .group, col = .date }) data
+
+-}
 levelsOf :
     LevelMap a comparable1 comparable2
     -> List a
@@ -157,41 +273,85 @@ levelsOf (LevelMap { row, col }) records =
         List.foldr accum ( Set.empty, Set.empty ) records
             |> finalize
 
+{-|
+
+Specify a _level mapping_: a pair of functions for extracting the row and
+column levels from a source record.
+
+Note that row and column levels can be any `comparable`, so this includes
+tuples of comparables. In this way you can build hierarchical row or column
+categories:
+
+    levelMap 
+        { row = (\car -> (car.make, car.purchaseCountry))
+        , col = (\car -> (car.year, if car.type == 'hybrid' then 1 else 0))
+        }
+
+-}
+levelMap : 
+    { x | row : a -> comparable1, col : a -> comparable2 }
+    -> LevelMap a comparable1 comparable2
+levelMap {row, col} =
+    LevelMap { row = row, col = col }
+
 
 
 -- ACCESSORS
 
+{-|
 
-levels : Crosstab a b comparable1 comparable2 -> Levels comparable1 comparable2
-levels (Crosstab { levels }) =
-    levels
+Get the list of row levels from a crosstab.
 
-
+-}
 rowLevelList : Crosstab a b comparable1 comparable2 -> List comparable1
 rowLevelList (Crosstab { levels }) =
     Array.toList levels.rows
 
 
+{-|
+
+Get the list of column levels from a crosstab.
+
+-}
 colLevelList : Crosstab a b comparable1 comparable2 -> List comparable2
 colLevelList (Crosstab { levels }) =
     Array.toList levels.cols
 
 
+{-|
+
+Get the list of values from a crosstab (rows of columns).
+
+-}
 rowList : Crosstab a b comparable1 comparable2 -> List (List a)
 rowList (Crosstab { values }) =
     toListMatrix values
 
+{-|
 
+Get the row summaries from a crosstab.
+
+-}
 rowSummaryList : Crosstab a b comparable1 comparable2 -> List b
 rowSummaryList (Crosstab { summary }) =
     Array.toList summary.rows
 
 
+{-|
+
+Get the column summaries from a crosstab.
+
+-}
 colSummaryList : Crosstab a b comparable1 comparable2 -> List b
 colSummaryList (Crosstab { summary }) =
     Array.toList summary.cols
 
 
+{-|
+
+Get the table summary ("grand total") from a crosstab.
+
+-}
 tableSummary : Crosstab a b comparable1 comparable2 -> b
 tableSummary (Crosstab { summary }) =
     summary.table
@@ -200,7 +360,32 @@ tableSummary (Crosstab { summary }) =
 
 -- OPERATIONS
 
+{-|
 
+Calculate row, column, and table summaries from the given crosstab using
+the given calculation, returning a new crosstab.
+
+In many cases you don't need this additional pass over the data and can do
+the necessary summary calculations as part of the initial load (`fromList`).
+But in principle you could use it to do a different calculation over the
+same crosstab.
+
+For instance, with something like this you could use the same underlying 
+crosstab data (stored as `Set a`)  to calculate _first_ the unique counts
+(`Set.size`), and _then_ the quantiles, without having to load the data
+in again:
+
+    uniqueCounts =
+        fromList 
+            (Crosstab.Calc.unique Set.size) 
+            (Crosstab.Calc.uniqueOf .y identity)
+            (levelMap { row = .x1, col = .x2 })
+            data
+        
+    quantiles =
+        calc ( quantiles [0.25,0.5,0.75] ) uniqueCounts
+
+-}
 calc :
     Calc a b c
     -> Crosstab a b comparable1 comparable2
@@ -212,6 +397,22 @@ calc summary (Crosstab { levels, values }) =
         , summary = calcValuesSummary summary values
         }
 
+{-|
+
+Compare crosstab values with their corresponding row, column, and table
+summaries using the given function, returning a new crosstab.
+
+Note you also must pass in a default value for the new crosstab. 
+
+Note also the summary calculations are left untouched.
+
+    let
+        rowPct {row} val =
+            val / row
+    in
+        compare RowPct 0.0 crosstab
+       
+-}
 compare :
     (Compare a b -> a -> c)
     -> c
@@ -224,7 +425,11 @@ compare comp init (Crosstab { levels, summary, values }) =
         , summary = summary
         }
 
+{-|
 
+Compare, and then calculate summaries with the results of the compare operation.
+
+-}
 compareAndCalc :
     (Compare a b -> a -> c)
     -> Calc c c d
@@ -248,7 +453,11 @@ compareAndCalc comp (Calc calc) (Crosstab { levels, summary, values }) =
 
 -- CALC CONSTRUCTORS
 
+{-|
 
+TODO
+
+-}
 customCalc :
     { x | map : b -> c, accum : a -> b -> b, init : b }
     -> Calc a b c
